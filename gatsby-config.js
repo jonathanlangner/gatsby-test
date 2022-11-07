5f451a8fc515c3f55822578b51217caa4c4be7b5
jonathanlangner <jonlangner@gmail.com>
2022-11-07 11:36:33 -0600

    First Commit.

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `Intrepid Weather`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-sitemap"]
+};
